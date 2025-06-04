c1505e603005d4c9096d9315615a4f176bde2ab6
Leo-Tanaka <163356145+Leo-Tanaka@users.noreply.github.com>
2025-06-04 08:43:09 -0300

    adaptado render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -41,4 +42,5 @@ def listar_status():
     return jsonify(status_data), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
